2ba13dbd1b2424359c98da991714f9cacec94a8c
Sahil Singhal <sahil@sahil-desktop.(none)>
2010-06-11 17:06:38 -0400

    Minor change.

--- qcsrc/menu/xonotic/dialog_hudpanel_options.c
+++ qcsrc/menu/xonotic/dialog_hudpanel_options.c
@@ -1,7 +1,7 @@
 #ifdef INTERFACE
 CLASS(XonoticHUDOptionsDialog) EXTENDS(XonoticRootDialog)
 	METHOD(XonoticHUDOptionsDialog, fill, void(entity))
-	ATTRIB(XonoticHUDOptionsDialog, title, string, "HUD Setup")
+	ATTRIB(XonoticHUDOptionsDialog, title, string, "Panel Options")
 	ATTRIB(XonoticHUDOptionsDialog, color, vector, SKINCOLOR_DIALOG_TEAMSELECT)
 	ATTRIB(XonoticHUDOptionsDialog, intendedWidth, float, 0.4)
 	ATTRIB(XonoticHUDOptionsDialog, rows, float, 15)
@@ -15,7 +15,17 @@ ENDCLASS(XonoticHUDOptionsDialog)
 void fillXonoticHUDOptionsDialog(entity me)
 {
 	entity e;
+	float panelID;
 	me.TR(me);
+	
+	panelID = cvar("_hud_panel_id");
+	if(panelID >= 0) 
+	{
+		me.TR(me);
+			me.TDempty(me, 0.2);
+			me.TD(me, 1, 1.2, e = makeNexuizTextLabel(0, "Alpha:"));
+			me.TD(me, 1, 2.6, e = makeNexuizSlider(0, 1, 0.01, "hud_", HUD_Panel_GetName(panelID), "_bg_alpha"));
+	}
 	
 }
 #endif
