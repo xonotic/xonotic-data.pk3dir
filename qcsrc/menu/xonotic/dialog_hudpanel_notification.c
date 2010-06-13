2424edd767271ead28c2847b3af2fa854d265a81
FruitieX <rasse@rasse-lappy.localdomain>
2010-06-14 00:27:11 +0300

    add (fade)time settings to notify dialog

--- qcsrc/menu/xonotic/dialog_hudpanel_notification.c
+++ qcsrc/menu/xonotic/dialog_hudpanel_notification.c
@@ -25,6 +25,14 @@ void fillXonoticHUDNotificationDialog(entity me)
 				me.TD(me, 1, 2, e = makeXonoticSlider(0, 1, 0.01, "hud_notify_bg_alpha"));
 		me.TR(me);
 			me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "hud_notify_flip", "Flip"));
+		me.TR(me);
+			me.TD(me, 1, 2, e = makeXonoticTextLabel(0, "Msg lifetime:"));
+				me.TD(me, 1, 2, e = makeXonoticSlider(3, 15, 1, "hud_notify_time"));
+		me.TR(me);
+			me.TD(me, 1, 2, e = makeXonoticTextLabel(0, "Msg fadetime:"));
+				me.TD(me, 1, 2, e = makeXonoticSlider(1, 5, 1, "hud_notify_fadetime"));
+		me.TR(me);
+			me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "hud_notify_print", "Also print messages to console"));
 	me.TR(me);
 	//me.gotoRC(me, me.rows - 1, 0);
 		//me.TD(me, 1, me.columns, e = makeXonoticCommandButton("Exit Setup", '0 0 0', "_hud_configure 0", 1));
