efac60a8d33ea137756b6436f1e0be933f88b2b4
Sahil Singhal <sahil@sahil-desktop.(none)>
2010-06-12 21:39:26 -0400

    Add mirror to healtharmor panel.

--- qcsrc/menu/xonotic/dialog_hudpanel_healtharmor.c
+++ qcsrc/menu/xonotic/dialog_hudpanel_healtharmor.c
@@ -25,6 +25,8 @@ void fillXonoticHUDHealthArmorDialog(entity me)
 				me.TD(me, 1, 2, e = makeXonoticSlider(0, 1, 0.01, "hud_healtharmor_bg_alpha"));
 		me.TR(me);
 			me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "hud_healtharmor_flip", "Flip"));
+		me.TR(me);
+			me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "hud_healtharmor_mirror", "Mirror"));
 	me.TR(me);
 	//me.gotoRC(me, me.rows - 1, 0);
 		//me.TD(me, 1, me.columns, e = makeXonoticCommandButton("Exit Setup", '0 0 0', "_hud_configure 0", 1));
