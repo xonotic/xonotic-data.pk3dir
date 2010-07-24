707b507ca6af727edb07ad82399c455a798de88c
FruitieX <rasse@rasse-lappy.localdomain>
2010-07-24 20:14:53 +0300

    change update-cvarcount.sh to use balanceXonotic.cfg

--- update-cvarcount.sh
+++ update-cvarcount.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-countw=`awk '/^seta? g_/ { print $2; }' balance.cfg       | sort -u | tr -d '\r' | git hash-object --stdin | cut -c 1-32`
+countw=`awk '/^seta? g_/ { print $2; }' balanceXonotic.cfg       | sort -u | tr -d '\r' | git hash-object --stdin | cut -c 1-32`
 for b in balance*.cfg; do
 	countb=`awk '/^seta? g_/ { print $2; }' "$b"  | sort -u | tr -d '\r' | git hash-object --stdin | cut -c 1-32`
 	if [ "$countw" != "$countb" ]; then
-		echo "Mismatch between balance.cfg and $b. Aborting."
+		echo "Mismatch between balanceXonotic.cfg and $b. Aborting."
 		exit 1
 	fi
 done
