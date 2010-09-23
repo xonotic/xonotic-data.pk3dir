323f6352438197f836a3de2eb4e6d86a8ca1b5b8
Rudolf Polzer <divVerent@alientrap.org>
2010-09-23 11:21:30 +0200

    update-cvarcount: report too much/missing cvars

--- update-cvarcount.sh
+++ update-cvarcount.sh
@@ -5,6 +5,13 @@ for b in balance*.cfg; do
 	countb=`awk '/^seta? g_/ { print $2; }' "$b"  | sort -u | tr -d '\r' | git hash-object --stdin | cut -c 1-32`
 	if [ "$countw" != "$countb" ]; then
 		echo "Mismatch between balanceXonotic.cfg and $b. Aborting."
+		echo "Differences are:"
+		A=`mktemp`
+		B=`mktemp`
+		awk '/^seta? g_/ { print $2; }' balanceXonotic.cfg       | sort -u | tr -d '\r' > "$A"
+		awk '/^seta? g_/ { print $2; }' "$b"                     | sort -u | tr -d '\r' > "$B"
+		diff "$A" "$B" | grep '^[<>]' | sort
+		rm -f "$A" "$B"
 		exit 1
 	fi
 done
