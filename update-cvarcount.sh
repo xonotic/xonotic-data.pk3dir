3adfd91c205d8b4fc0b66b590c4d552905114ffc
Rudolf Polzer <divVerent@alientrap.org>
2010-09-23 11:23:33 +0200

    explain more

--- update-cvarcount.sh
+++ update-cvarcount.sh
@@ -10,6 +10,8 @@ for b in balance*.cfg; do
 		B=`mktemp`
 		awk '/^seta? g_/ { print $2; }' balanceXonotic.cfg       | sort -u | tr -d '\r' > "$A"
 		awk '/^seta? g_/ { print $2; }' "$b"                     | sort -u | tr -d '\r' > "$B"
+		echo "< missing in $b"
+		echo "> must get removed from $b"
 		diff "$A" "$B" | grep '^[<>]' | sort
 		rm -f "$A" "$B"
 		exit 1
