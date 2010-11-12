78b09b7243662c83f1870fe8a2dc268b0e5d43bd
FruitieX <fruitiex@gmail.com>
2010-11-12 09:24:46 +0200

    implement a crosshair preview in the playersetup dialog

--- qcsrc/menu/xonotic/crosshairbutton.c
+++ qcsrc/menu/xonotic/crosshairbutton.c
@@ -33,11 +33,15 @@ void XonoticCrosshairButton_configureXonoticCrosshairButton(entity me, float the
 	me.loadCvars(me);
 	me.configureRadioButton(me, string_null, me.fontSize, me.image, theGroup, 0);
 	me.srcMulti = 1;
-	me.src3 = strzone(strcat("/gfx/crosshair", ftos(me.cvarValueFloat)));
+	if(me.cvarValueFloat == -1)
+		me.src3 = strzone(strcat("/gfx/crosshair", cvar("crosshair")));
+	else
+		me.src3 = strzone(strcat("/gfx/crosshair", ftos(me.cvarValueFloat)));
 	me.src4 = "/gfx/crosshairdot";
 }
 void XonoticCrosshairButton_setChecked(entity me, float val)
 {
+	if(me.cvarValueFloat != -1) // preview shouldn't work as a button
 	if(val != me.checked)
 	{
 		me.checked = val;
@@ -68,21 +72,34 @@ void XonoticCrosshairButton_draw(entity me)
 	rgb = stov(cvar_string("crosshair_color"));
 	a = cvar("crosshair_alpha");
 
-	if(!me.checked && !me.focused)
+	if(!me.checked && !me.focused && me.cvarValueFloat != -1)
 	{
 		a *= me.disabledAlpha;
 		rgb = '1 1 1';
+	}
+
+	if(me.cvarValueFloat == -1) // update the preview if this is the preview button
+	{
+		if(me.src3)
+			strunzone(me.src3);
+		me.src3 = strzone(strcat("/gfx/crosshair", cvar_string("crosshair")));
+		me.focused = 1;
 	}
 
 	SUPER(XonoticCrosshairButton).draw(me);
 
 	sz = draw_PictureSize(me.src3);
 	sz = globalToBoxSize(sz, draw_scale);
-	sz = (10 * '1 1 0' + sz * cvar("crosshair_size")) * 0.05; // (10 * '1 1 0' + ...) * 0.05 here to make visible size changes happen also at bigger sizes
-	if(sz_x > 0.95)
-		sz = sz * (0.95 / sz_x);
-	if(sz_y > 0.95)
-		sz = sz * (0.95 / sz_y);
+	if(me.cvarValueFloat == -1)
+	{
+		sz = (6 * '1 1 0' + sz * cvar("crosshair_size")) * 0.08; // (6 * '1 1 0' + ...) * 0.08 here to make visible size changes happen also at bigger sizes
+		if(sz_x > 0.95)
+			sz = sz * (0.95 / sz_x);
+		if(sz_y > 0.95)
+			sz = sz * (0.95 / sz_y);
+	}
+	else // show the crosshair picker at full size
+		sz = '0.95 0.95 0';
 
 	draw_Picture('0.5 0.5 0' - 0.5 * sz, me.src3, sz, rgb, a);
 	if(cvar("crosshair_dot"))
