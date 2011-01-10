0f34869cd749a88433c2d39074c73b6dda1287c7
terencehill <piuntn@gmail.com>
2011-01-10 21:42:37 +0100

    Make PlayerSetup-Waypoints dialog smaller, remove redundant "Waypoint settings:" label as the dialog only contains waypoints settings, raise step to change alpha/scale from 0.01 to 0.05 as a difference of 0.01 is unnoticeable

--- qcsrc/menu/xonotic/dialog_multiplayer_playersetup_waypoint.c
+++ qcsrc/menu/xonotic/dialog_multiplayer_playersetup_waypoint.c
@@ -5,16 +5,16 @@ CLASS(XonoticWaypointDialog) EXTENDS(XonoticDialog)
 	METHOD(XonoticWaypointDialog, showNotify, void(entity))
 	ATTRIB(XonoticWaypointDialog, title, string, "Waypoints")
 	ATTRIB(XonoticWaypointDialog, color, vector, SKINCOLOR_DIALOG_RADAR)
-	ATTRIB(XonoticWaypointDialog, intendedWidth, float, 0.7)
+	ATTRIB(XonoticWaypointDialog, intendedWidth, float, 0.5)
 	ATTRIB(XonoticWaypointDialog, rows, float, 6)
-	ATTRIB(XonoticWaypointDialog, columns, float, 4)
+	ATTRIB(XonoticWaypointDialog, columns, float, 3)
 ENDCLASS(XonoticWaypointDialog)
 #endif
 
 #ifdef IMPLEMENTATION
 void XonoticWaypointDialog_showNotify(entity me)
 {
-        loadAllCvars(me);
+	loadAllCvars(me);
 }
 string XonoticWaypointDialog_toString(entity me)
 {
@@ -23,26 +23,21 @@ string XonoticWaypointDialog_toString(entity me)
 void XonoticWaypointDialog_fill(entity me)
 {
 	entity e;
-	
+
 	me.TR(me);
-		me.TD(me, 1, 4, makeXonoticTextLabel(0, "Waypoint settings:"));
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(1, "cl_hidewaypoints", "Show base waypoints"));
 	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 3.8, e = makeXonoticCheckBox(1, "cl_hidewaypoints", "Show base waypoints"));
+		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, "Waypoint scale:"));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0.5, 1.5, 0.05, "g_waypointsprite_scale"));
+			setDependent(e, "cl_hidewaypoints", 0, 0);
 	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, "Waypoint scale:"));
-			me.TD(me, 1, 3, e = makeXonoticSlider(0.5, 1.5, 0.01, "g_waypointsprite_scale"));
-				setDependent(e, "cl_hidewaypoints", 0, 0);
+		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, "Waypoint alpha:"));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0.1, 1, 0.05, "g_waypointsprite_alpha"));
+			setDependent(e, "cl_hidewaypoints", 0, 0);
 	me.TR(me);
- 		me.TDempty(me, 0.2);
-		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, "Waypoint alpha:"));
-			me.TD(me, 1, 3, e = makeXonoticSlider(0.1, 1, 0.01, "g_waypointsprite_alpha"));
-				setDependent(e, "cl_hidewaypoints", 0, 0);
 	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, "Show names:"));
-		me.TD(me, 1, 3, e = makeXonoticTextSlider("cl_shownames"));
+		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, "Show names:"));
+		me.TD(me, 1, 2, e = makeXonoticTextSlider("cl_shownames"));
 			e.addValue(e, "Never", "0");
 			e.addValue(e, "Teammates", "1");
 			e.addValue(e, "All players", "2");
@@ -52,5 +47,4 @@ void XonoticWaypointDialog_fill(entity me)
 			e.onClick = Dialog_Close;
 			e.onClickEntity = me;
 }
-
 #endif
