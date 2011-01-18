6928cee70fe3f2701e953e4fdd7fa556c97ed59e
Rudolf Polzer <divverent@alientrap.org>
2011-01-18 21:26:23 +0100

    translation checker: make it run on CSQC too

--- check-translations.sh
+++ check-translations.sh
@@ -1,14 +1,16 @@
-find qcsrc/menu -type f -not -name \*.po -not -name \*.txt | xgettext -LC -k_ -f- --from-code utf-8 -o menu.dat.pot >&2
-
-for X in menu.dat.*.po; do
-	msgmerge -U "$X" menu.dat.pot >&2
-	todo=$(
-		msgattrib --untranslated "$X" | grep -A 2147483647 "^#:"
-		msgattrib --fuzzy "$X"        | grep -A 2147483647 "^#:"
-	)
-	if [ -n "$todo" ]; then
-		echo "TODO for translation $X:"
-		echo "$todo"
-		echo
-	fi
+for VM in menu client; do
+	find qcsrc/"$VM" -type f -not -name \*.po -not -name \*.txt | xgettext -LC -k_ -f- --from-code utf-8 -o "$VM".dat.pot >&2
+	for X in "$VM".dat.*.po; do
+		[ -f "$X" ] || continue
+		msgmerge -U "$X" "$VM".dat.pot >&2
+		todo=$(
+			msgattrib --untranslated "$X" | grep -A 2147483647 "^#:"
+			msgattrib --fuzzy "$X"        | grep -A 2147483647 "^#:"
+		)
+		if [ -n "$todo" ]; then
+			echo "TODO for translation $X:"
+			echo "$todo"
+			echo
+		fi
+	done
 done
