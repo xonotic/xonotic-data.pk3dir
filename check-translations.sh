f8cad00f98f75aac2ccdbc8bfc24b7a418ccd0e1
Rudolf Polzer <divverent@alientrap.org>
2011-01-18 21:36:06 +0100

    sort the message catalogs

--- check-translations.sh
+++ check-translations.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 for VM in menu client; do
 	find qcsrc/"$VM" -type f -not -name \*.po -not -name \*.txt | xgettext -LC -k_ -f- --from-code utf-8 -o "$VM".dat.pot >&2
 	for X in "$VM".dat.*.po; do
 		[ -f "$X" ] || continue
-		msgmerge -U "$X" "$VM".dat.pot >&2
+		msgmerge -F -U "$X" "$VM".dat.pot >&2
 		todo=$(
 			msgattrib --untranslated "$X" | grep -A 2147483647 "^#:"
 			msgattrib --fuzzy "$X"        | grep -A 2147483647 "^#:"
