92982a7c62b989146cd802754aec73f2051d5be0
FruitieX <fruitiex@gmail.com>
2011-04-03 00:15:08 +0300

    quick implementation of shownames in the HUD. Currently networked via MSG_ONE, not sure that's such a good thing...

--- qcsrc/menu/classes.c
+++ qcsrc/menu/classes.c
@@ -102,5 +102,6 @@
 #include "xonotic/dialog_hudpanel_chat.c"
 #include "xonotic/dialog_hudpanel_engineinfo.c"
 #include "xonotic/dialog_hudpanel_infomessages.c"
+#include "xonotic/dialog_hudpanel_shownames.c"
 #include "xonotic/dialog_hudpanel_weapons.c"
 #include "xonotic/slider_picmip.c"
