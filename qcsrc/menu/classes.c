eae99a40c8cd424391c9ee97f8106c4bb4c262a9
terencehill <piuntn@gmail.com>
2011-06-25 18:42:24 +0200

    Menu dialog for the centerprint panel

--- qcsrc/menu/classes.c
+++ qcsrc/menu/classes.c
@@ -104,4 +104,5 @@
 #include "xonotic/dialog_hudpanel_infomessages.c"
 #include "xonotic/dialog_hudpanel_weapons.c"
 #include "xonotic/dialog_hudpanel_physics.c"
+#include "xonotic/dialog_hudpanel_centerprint.c"
 #include "xonotic/slider_picmip.c"
