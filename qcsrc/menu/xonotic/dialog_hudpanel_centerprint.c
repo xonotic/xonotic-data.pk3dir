25969a0c39d5693caccf2269ae67576ec4985510
terencehill <piuntn@gmail.com>
2011-06-28 20:04:55 +0200

    hud_panel_centerprint_fadetime to customize the msg fade time

--- qcsrc/menu/xonotic/dialog_hudpanel_centerprint.c
+++ qcsrc/menu/xonotic/dialog_hudpanel_centerprint.c
@@ -24,6 +24,10 @@ void XonoticHUDCenterprintDialog_fill(entity me)
 		me.TD(me, 1, 2.6, e = makeXonoticSlider(1, 10, 1, "hud_panel_centerprint_time"));
 	me.TR(me);
 		me.TDempty(me, 0.2);
+		me.TD(me, 1, 1.2, e = makeXonoticTextLabel(0, _("Fade time:")));
+		me.TD(me, 1, 2.6, e = makeXonoticSlider(0, 1, 0.05, "hud_panel_centerprint_fadetime"));
+	me.TR(me);
+		me.TDempty(me, 0.2);
 		me.TD(me, 1, 3.8, e = makeXonoticCheckBox(0, "hud_panel_centerprint_flip", _("Flip messages order")));
 	me.TR(me);
 		me.TD(me, 1, 4, e = makeXonoticTextLabel(0, _("Text alignment:")));
