55626a0e86edc9d2c0776f45eb7ccba14f84c254
Rudolf Polzer <divVerent@xonotic.org>
2011-08-14 11:24:19 +0200

    fix spelling Race/CTS

--- qcsrc/menu/xonotic/dialog_hudpanel_physics.c
+++ qcsrc/menu/xonotic/dialog_hudpanel_physics.c
@@ -22,7 +22,7 @@ void XonoticHUDPhysicsDialog_fill(entity me)
 			e.addValue(e, _("Panel disabled"), "0");
 			e.addValue(e, _("Panel enabled"), "1");
 			e.addValue(e, _("Panel enabled even observing"), "2");
-			e.addValue(e, _("Panel enabled only in race/cts"), "3");
+			e.addValue(e, _("Panel enabled only in Race/CTS"), "3");
 			e.configureXonoticTextSliderValues(e);
 
 	DIALOG_HUDPANEL_COMMON_NOTOGGLE();
