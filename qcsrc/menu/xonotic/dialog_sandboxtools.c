19f327de06effd70c90012cd1ae99efabd3a6e71
Mircea Kitsune <sonichedgehog_hyperblast00@yahoo.com>
2011-10-25 15:55:39 +0300

    First functionality for the sandbox menu. Allows specifying the model in an input field, and spawning the object by button.

--- qcsrc/menu/xonotic/dialog_sandboxtools.c
+++ qcsrc/menu/xonotic/dialog_sandboxtools.c
@@ -3,8 +3,8 @@ CLASS(XonoticSandboxToolsDialog) EXTENDS(XonoticRootDialog)
 	METHOD(XonoticSandboxToolsDialog, fill, void(entity)) // to be overridden by user to fill the dialog with controls
 	ATTRIB(XonoticSandboxToolsDialog, title, string, _("Sandbox Tools")) // ;)
 	ATTRIB(XonoticSandboxToolsDialog, color, vector, SKINCOLOR_DIALOG_SANDBOXTOOLS)
-	ATTRIB(XonoticSandboxToolsDialog, intendedWidth, float, 0.4)
-	ATTRIB(XonoticSandboxToolsDialog, rows, float, 5)
+	ATTRIB(XonoticSandboxToolsDialog, intendedWidth, float, 0.8)
+	ATTRIB(XonoticSandboxToolsDialog, rows, float, 20)
 	ATTRIB(XonoticSandboxToolsDialog, columns, float, 4)
 	ATTRIB(XonoticSandboxToolsDialog, name, string, "SandboxTools")
 ENDCLASS(XonoticSandboxToolsDialog)
@@ -13,7 +13,16 @@ ENDCLASS(XonoticSandboxToolsDialog)
 #ifdef IMPLEMENTATION
 void XonoticSandboxToolsDialog_fill(entity me)
 {
-	entity e;
+	entity e, box;
+
+	me.TR(me);
+		me.TD(me, 1, 0.25, e = makeXonoticTextLabel(0, _("Model:")));
+		me.TD(me, 1, 2.0, box = makeXonoticInputBox(1, "menu_sandbox_spawn_model"));
+			box.forbiddenCharacters = "\r\n\\\"$"; // don't care, isn't getting saved
+			box.maxLength = -127; // negative means encoded length in bytes
+			box.saveImmediately = 1;
+	me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.5, makeXonoticCommandButton(_("Spawn"), '0 0 0', "sandbox spawn_object $menu_sandbox_spawn_model", 0));
 
 	me.gotoRC(me, me.rows - 1, 0);
 		me.TD(me, 1, me.columns, e = makeXonoticButton(_("OK"), '0 0 0'));
