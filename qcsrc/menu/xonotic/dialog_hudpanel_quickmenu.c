ad33c9a79b11c8f1119f8928bf6f22d81bba2177
terencehill <piuntn@gmail.com>
2011-12-03 00:06:26 +0100

    Allow to change text alignment in the quick menu

--- qcsrc/menu/xonotic/dialog_hudpanel_quickmenu.c
+++ qcsrc/menu/xonotic/dialog_hudpanel_quickmenu.c
@@ -17,5 +17,13 @@ void XonoticHUDQuickMenuDialog_fill(entity me)
 	string panelname = "quickmenu";
 
 	DIALOG_HUDPANEL_COMMON();
+
+	me.TR(me);
+		me.TD(me, 1, 4, e = makeXonoticTextLabel(0, _("Text alignment:")));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+			me.TD(me, 1, 3.8/3, e = makeXonoticRadioButton(3, "hud_panel_quickmenu_align", "0", _("Left")));
+			me.TD(me, 1, 3.8/3, e = makeXonoticRadioButton(3, "hud_panel_quickmenu_align", "0.5", _("Center")));
+			me.TD(me, 1, 3.8/3, e = makeXonoticRadioButton(3, "hud_panel_quickmenu_align", "1", _("Right")));
 }
 #endif
