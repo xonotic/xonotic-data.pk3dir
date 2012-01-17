63c0c27cb12b812257d89a7a45b8451eab341e0b
Samual <samual@xonotic.org>
2012-01-16 21:01:32 -0500

    WHY DO I ALWAYS FORGET THIS! haha

---
+++ qcsrc/menu/xonotic/dialog_multiplayer_playersetup_crosshair.c
@@ -0,0 +1,34 @@
+#ifdef INTERFACE
+CLASS(XonoticCrosshairDialog) EXTENDS(XonoticDialog)
+	METHOD(XonoticCrosshairDialog, toString, string(entity))
+	METHOD(XonoticCrosshairDialog, fill, void(entity))
+	METHOD(XonoticCrosshairDialog, showNotify, void(entity))
+	ATTRIB(XonoticCrosshairDialog, title, string, _("Crosshair settings"))
+	ATTRIB(XonoticCrosshairDialog, color, vector, SKINCOLOR_DIALOG_CROSSHAIR)
+	ATTRIB(XonoticCrosshairDialog, intendedWidth, float, 0.5)
+	ATTRIB(XonoticCrosshairDialog, rows, float, 12)
+	ATTRIB(XonoticCrosshairDialog, columns, float, 3)
+ENDCLASS(XonoticCrosshairDialog)
+#endif
+
+#ifdef IMPLEMENTATION
+void XonoticCrosshairDialog_showNotify(entity me)
+{
+	loadAllCvars(me);
+}
+string XonoticCrosshairDialog_toString(entity me)
+{
+	return "hi"; //me.weaponsList.toString(me.weaponsList);
+}
+void XonoticCrosshairDialog_fill(entity me)
+{
+	entity e;
+	
+
+		
+	me.gotoRC(me, me.rows - 1, 0);
+		me.TD(me, 1, me.columns, e = makeXonoticButton(_("OK"), '0 0 0'));
+			e.onClick = Dialog_Close;
+			e.onClickEntity = me;
+}
+#endif
---
+++ qcsrc/menu/xonotic/dialog_multiplayer_playersetup_model.c
@@ -0,0 +1,34 @@
+#ifdef INTERFACE
+CLASS(XonoticModelDialog) EXTENDS(XonoticDialog)
+	METHOD(XonoticModelDialog, toString, string(entity))
+	METHOD(XonoticModelDialog, fill, void(entity))
+	METHOD(XonoticModelDialog, showNotify, void(entity))
+	ATTRIB(XonoticModelDialog, title, string, _("Model settings"))
+	ATTRIB(XonoticModelDialog, color, vector, SKINCOLOR_DIALOG_MODEL)
+	ATTRIB(XonoticModelDialog, intendedWidth, float, 0.5)
+	ATTRIB(XonoticModelDialog, rows, float, 12)
+	ATTRIB(XonoticModelDialog, columns, float, 3)
+ENDCLASS(XonoticModelDialog)
+#endif
+
+#ifdef IMPLEMENTATION
+void XonoticModelDialog_showNotify(entity me)
+{
+	loadAllCvars(me);
+}
+string XonoticModelDialog_toString(entity me)
+{
+	return "hi"; //me.weaponsList.toString(me.weaponsList);
+}
+void XonoticModelDialog_fill(entity me)
+{
+	entity e;
+	
+
+		
+	me.gotoRC(me, me.rows - 1, 0);
+		me.TD(me, 1, me.columns, e = makeXonoticButton(_("OK"), '0 0 0'));
+			e.onClick = Dialog_Close;
+			e.onClickEntity = me;
+}
+#endif
