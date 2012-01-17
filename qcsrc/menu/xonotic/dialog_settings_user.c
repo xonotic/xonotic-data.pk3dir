2447a648010748a3589518ec08cbba9990dd3828
Samual <samual@xonotic.org>
2012-01-17 02:28:32 -0500

    Working on user tab

--- qcsrc/menu/xonotic/dialog_settings_user.c
+++ qcsrc/menu/xonotic/dialog_settings_user.c
@@ -4,7 +4,7 @@ CLASS(XonoticUserSettingsTab) EXTENDS(XonoticTab)
 	ATTRIB(XonoticUserSettingsTab, title, string, _("User"))
 	ATTRIB(XonoticUserSettingsTab, intendedWidth, float, 0.9)
 	ATTRIB(XonoticUserSettingsTab, rows, float, 17)
-	ATTRIB(XonoticUserSettingsTab, columns, float, 6.2) // added extra .2 for center space 
+	ATTRIB(XonoticUserSettingsTab, columns, float, 5)
 ENDCLASS(XonoticUserSettingsTab)
 entity makeXonoticUserSettingsTab();
 #endif
@@ -21,38 +21,43 @@ entity makeXonoticUserSettingsTab()
 void XonoticUserSettingsTab_fill(entity me)
 {
 	entity e;
+	entity sk;
 
 	me.TR(me);
-		//me.TD(me, 1, 3, e = makeXonoticCheckBox(1, "cl_nolerp", _("User update smoothing")));
+		me.TD(me, 1, 2, e = makeXonoticTextLabel(0, _("Menu skins:")));
 	me.TR(me);
-		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "shownetgraph", _("Show netgraph")));
+		me.TD(me, me.rows - 2, 2, sk = makeXonoticSkinList());
+	me.gotoRC(me, me.rows - 1, 0);
+		me.TD(me, 1, 2, e = makeXonoticButton(_("Apply immediately"), '0 0 0'));
+			e.onClick = SetSkin_Click;
+			e.onClickEntity = sk;
+
+	me.gotoRC(me, 0, 2.4); me.setFirstColumn(me, me.currentColumn);
+		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("Text language:")));
 	me.TR(me);
-	me.TR(me);
-		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("User speed:")));
-		me.TD(me, 1, 2, e = makeXonoticTextSlider("_cl_rate"));
-			e.addValue(e, _("56k"), "4000");
-			e.addValue(e, _("ISDN"), "7000");
-			e.addValue(e, _("Slow ADSL"), "15000");
-			e.addValue(e, _("Fast ADSL"), "20000");
-			e.addValue(e, _("Broadband"), "66666");
-			e.configureXonoticTextSliderValues(e);
-    me.TR(me);
-		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("Input packets/s:")));
-		me.TD(me, 1, 2, e = makeXonoticSlider(20, 100, 1, "cl_netfps"));
+		me.TD(me, 6, 1, sk = makeXonoticLanguageList());
 	me.TR(me);
 	me.TR(me);
-		me.TD(me, 1, 3, e = makeXonoticTextLabel(0, _("HTTP downloads:")));
-	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Downloads:")));
-		me.TD(me, 1, 2, e = makeXonoticSlider(1, 5, 1, "cl_curl_maxdownloads"));
-	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Speed (kB/s):")));
-		me.TD(me, 1, 2, e = makeXonoticSlider(10, 1500, 10, "cl_curl_maxspeed"));
 	me.TR(me);
 	me.TR(me);
-		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("Client UDP port:")));
-		me.TD(me, 1, 0.64, e = makeXonoticInputBox(0, "cl_port"));
+	me.TR(me);
+	me.TR(me);
+		me.TD(me, 1, 1, e = makeXonoticButton(_("Set language"), '0 0 0'));
+			e.onClick = SetLanguage_Click;
+			e.onClickEntity = sk;
+
+	me.gotoRC(me, 0, 3.6); me.setFirstColumn(me, me.currentColumn);
+		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("Font:")));
+		
+	me.gotoRC(me, 7, 2.2); me.setFirstColumn(me, me.currentColumn);
+	if(cvar("developer"))
+	{
+		me.TR(me);
+		me.TR(me);
+			me.TD(me, 1, 0.75, e = makeXonoticTextLabel(0, _("Menu tooltips:")));
+			me.TD(me, 1, 0.75, e = makeXonoticRadioButton(2, "menu_tooltips", "0", _("Disabled")));
+			me.TD(me, 1, 0.75, e = makeXonoticRadioButton(2, "menu_tooltips", "1", _("Standard")));
+			me.TD(me, 1, 0.75, e = makeXonoticRadioButton(2, "menu_tooltips", "2", _("Advanced")));
+	}
 }
 #endif
