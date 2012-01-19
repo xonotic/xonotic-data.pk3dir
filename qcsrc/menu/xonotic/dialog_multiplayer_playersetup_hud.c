7a91f3996d6efff260a66013d9b4681f0d481874
Samual <samual@xonotic.org>
2012-01-19 12:30:30 -0500

    Finish making the HUD settings dialog

--- qcsrc/menu/xonotic/dialog_multiplayer_playersetup_hud.c
+++ qcsrc/menu/xonotic/dialog_multiplayer_playersetup_hud.c
@@ -6,7 +6,7 @@ CLASS(XonoticHUDDialog) EXTENDS(XonoticDialog)
 	ATTRIB(XonoticHUDDialog, title, string, _("HUD settings"))
 	ATTRIB(XonoticHUDDialog, color, vector, SKINCOLOR_DIALOG_HUD)
 	ATTRIB(XonoticHUDDialog, intendedWidth, float, 0.5)
-	ATTRIB(XonoticHUDDialog, rows, float, 12)
+	ATTRIB(XonoticHUDDialog, rows, float, 18)
 	ATTRIB(XonoticHUDDialog, columns, float, 3)
 ENDCLASS(XonoticHUDDialog)
 void HUDSetup_Start(entity me, entity btn);
@@ -36,6 +36,55 @@ void XonoticHUDDialog_fill(entity me)
 {
 	entity e;
 	
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeXonoticTextLabel(0, _("Damage:")));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Overlay:")));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0, 1, 0.05, "hud_damage"));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Factor:")));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0.005, 0.1, 0.005, "hud_damage_factor"));
+		setDependent(e, "hud_damage", 0.001, 100);
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Fade rate:")));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0.25, 1, 0.05, "hud_damage_fade_rate"));
+		setDependent(e, "hud_damage", 0.001, 100);
+	me.TR(me);
+	
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(1, "cl_hidewaypoints", _("Waypoints")));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Scale:")));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0.5, 1.5, 0.05, "g_waypointsprite_scale"));
+		setDependent(e, "cl_hidewaypoints", 0, 0);
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Alpha:")));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0.1, 1, 0.05, "g_waypointsprite_alpha"));
+		setDependent(e, "cl_hidewaypoints", 0, 0);
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeXonoticTextLabel(0, _("Edge offset:")));
+		me.TD(me, 1, 2, e = makeXonoticSlider(0, 0.3, 0.01, "g_waypointsprite_edgeoffset_bottom"));
+		makeMulti(e, "g_waypointsprite_edgeoffset_top");
+		makeMulti(e, "g_waypointsprite_edgeoffset_left");
+		makeMulti(e, "g_waypointsprite_edgeoffset_right");
+		setDependent(e, "cl_hidewaypoints", 0, 0);
+	me.TR(me);
+	
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "hud_shownames", _("Show names above players")));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 2.8, e = makeXonoticCheckBoxEx(25, 0, "hud_shownames_crosshairdistance", _("Only when near crosshair")));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 2.8, e = makeXonoticCheckBox(0, "hud_shownames_status", _("Display health and armor")));
+	me.TR(me);
 	me.TR(me);
 		me.TDempty(me, 0.5);
 		me.TD(me, 1, 2, e = makeXonoticButton(_("Enter HUD editor"), '0 0 0'));
