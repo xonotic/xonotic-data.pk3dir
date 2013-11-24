d2bfab54471d800bb2477a6b184cfca7f2172684
Samual Lenks <samual@xonotic.org>
2013-11-24 15:15:51 -0500

    Remake fraglimit slider

--- qcsrc/menu/xonotic/dialog_multiplayer_create.c
+++ qcsrc/menu/xonotic/dialog_multiplayer_create.c
@@ -10,13 +10,45 @@ CLASS(XonoticServerCreateTab) EXTENDS(XonoticTab)
 	ATTRIB(XonoticServerCreateTab, mapListBox, entity, NULL)
 	ATTRIB(XonoticServerCreateTab, sliderFraglimit, entity, NULL)
 	ATTRIB(XonoticServerCreateTab, sliderTimelimit, entity, NULL)
-	ATTRIB(XonoticServerCreateTab, checkboxFraglimit, entity, NULL)
-	ATTRIB(XonoticServerCreateTab, checkboxFraglimitMapinfo, entity, NULL)
+	ATTRIB(XonoticServerCreateTab, labelFraglimit, entity, NULL)
 ENDCLASS(XonoticServerCreateTab)
 entity makeXonoticServerCreateTab();
 #endif
 
 #ifdef IMPLEMENTATION
+
+void GameType_ConfigureSliders(entity e, entity l, string pLabel, float pMin, float pMax, float pStep, string pCvar)
+{
+	if(pCvar == "")
+	{
+		e.configureXonoticTextSlider(e, string_null);
+		l.setText(l, pLabel);
+		e.disabled = l.disabled = TRUE;
+	}
+	else
+	{
+		e.configureXonoticTextSlider(e, pCvar);
+		
+		// clear old values
+		float i;
+		for(i = 0; i <= e.nValues; ++i);
+		{
+			if(e.(valueStrings[i])) { strunzone(e.(valueStrings[i])); }
+			if(e.(valueIdentifiers[i])) { strunzone(e.(valueIdentifiers[i])); }
+		}
+		e.clearValues(e);
+
+		// set new values
+		e.addValue(e, strzone(_("Default")), strzone("-1"));
+		for(i = pMin; i <= pMax; i += pStep) { e.addValue(e, strzone(ftos(i)), strzone(ftos(i))); }
+		e.addValue(e, strzone(_("Unlimited")), strzone("0"));
+		e.configureXonoticTextSliderValues(e);
+
+		// set text field
+		l.setText(l, pLabel);
+		e.disabled = l.disabled = FALSE;
+	}
+}
 
 entity makeXonoticServerCreateTab()
 {
@@ -45,21 +77,33 @@ void XonoticServerCreateTab_fill(entity me)
 	me.TR(me);
 		//me.TD(me, 1, 3, e = makeXonoticTextLabel(0, _("Match settings:"))); // pointless, overcrowds the dialog imo
 	me.TR(me);
-		me.sliderTimelimit = makeXonoticSlider(1.0, 60.0, 1, "timelimit_override");
-		me.TD(me, 1, 1, e = makeXonoticSliderCheckBox(0, 1, me.sliderTimelimit, _("Time limit:")));
-		me.TD(me, 1, 2, me.sliderTimelimit);
+		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("Time limit:")));
+		me.TD(me, 1, 2, e = makeXonoticTextSlider("timelimit_override"));
+			e.addValue(e, _("Default"), "-1");
+			e.addValue(e, "1", "1");
+			e.addValue(e, "2", "2");
+			e.addValue(e, "3", "3");
+			e.addValue(e, "4", "4");
+			e.addValue(e, "5", "5");
+			e.addValue(e, "6", "6");
+			e.addValue(e, "7", "7");
+			e.addValue(e, "8", "8");
+			e.addValue(e, "9", "9");
+			e.addValue(e, "10", "10");
+			e.addValue(e, "15", "15");
+			e.addValue(e, "20", "20");
+			e.addValue(e, "25", "25");
+			e.addValue(e, "30", "30");
+			e.addValue(e, "40", "40");
+			e.addValue(e, "50", "50");
+			e.addValue(e, "60", "60");
+			e.addValue(e, _("Infinite"), "0");
+			e.configureXonoticTextSliderValues(e);
 	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 2.8, e = makeXonoticSliderCheckBox(-1, 0, me.sliderTimelimit, _("Use map specified default")));
-	me.TR(me);
-		me.sliderFraglimit = makeXonoticSlider(1.0, 2000.0, 5, "fraglimit_override");
-		me.TD(me, 1, 1, e = makeXonoticSliderCheckBox(0, 1, me.sliderFraglimit, _("Point limit:")));
-			me.checkboxFraglimit = e;
-		me.TD(me, 1, 2, me.sliderFraglimit);
-	me.TR(me);
-		me.TDempty(me, 0.2);
-		me.TD(me, 1, 2.8, e = makeXonoticSliderCheckBox(-1, 0, me.sliderFraglimit, _("Use map specified default")));
-			me.checkboxFraglimitMapinfo = e;
+		me.TD(me, 1, 1, me.labelFraglimit = makeXonoticTextLabel(0, _("Frag limit:")));
+		me.TD(me, 1, 2, me.sliderFraglimit = makeXonoticTextSlider("fraglimit_override"));
+			GameType_ConfigureSliders(me.sliderFraglimit, me.labelFraglimit, _("Frag limit:"), 5, 100, 5, "fraglimit_override");
+
 	me.TR(me);
 		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("Player slots:")));
 		me.TD(me, 1, 2, makeXonoticSlider(1, 32, 1, "menu_maxplayers"));
@@ -124,43 +168,26 @@ void XonoticServerCreateTab_fill(entity me)
 	me.gameTypeChangeNotify(me);
 }
 
-void GameType_ConfigureSliders(entity e, entity l, entity l2, string pLabel, float pMin, float pMax, float pStep, string pCvar)
-{
-	if(pCvar == "")
-	{
-		e.configureXonoticSlider(e, pMin, pMax, pStep, string_null);
-		l.setText(l, pLabel);
-		e.disabled = l.disabled = l2.disabled = TRUE;
-	}
-	else
-	{
-		e.configureXonoticSlider(e, pMin, pMax, pStep, pCvar);
-		l.setText(l, pLabel);
-		e.disabled = l.disabled = l2.disabled = FALSE;
-	}
-}
-
 void XonoticServerCreateTab_gameTypeChangeNotify(entity me)
 {
 	// tell the map list to update
 	float gt;
-	entity e, l, l2;
+	entity e, l;
 	gt = MapInfo_CurrentGametype();
 	e = me.sliderFraglimit;
-	l = me.checkboxFraglimit;
-	l2 = me.checkboxFraglimitMapinfo;
+	l = me.labelFraglimit;
 	switch(gt)
 	{
-		case MAPINFO_TYPE_CTF:        GameType_ConfigureSliders(e, l, l2, _("Capture limit:"),   1,   20, 1, "capturelimit_override");     break;
-		case MAPINFO_TYPE_DOMINATION: GameType_ConfigureSliders(e, l, l2, _("Point limit:"),    50,  500, 10, "g_domination_point_limit"); break;
-		case MAPINFO_TYPE_KEYHUNT:    GameType_ConfigureSliders(e, l, l2, _("Point limit:"),   200, 1500, 50, "g_keyhunt_point_limit");    break;
-		case MAPINFO_TYPE_LMS:        GameType_ConfigureSliders(e, l, l2, _("Lives:"),           3,   50,  1, "g_lms_lives_override");     break;
-		case MAPINFO_TYPE_RACE:       GameType_ConfigureSliders(e, l, l2, _("Laps:"),            1,   25,  1, "g_race_laps_limit");        break;
-		case MAPINFO_TYPE_NEXBALL:    GameType_ConfigureSliders(e, l, l2, _("Goals:"),           1,   50,  1, "g_nexball_goallimit");      break;
-		case MAPINFO_TYPE_ASSAULT:    GameType_ConfigureSliders(e, l, l2, _("Point limit:"),    50,  500, 10, "");                         break;
-		case MAPINFO_TYPE_ONSLAUGHT:  GameType_ConfigureSliders(e, l, l2, _("Point limit:"),    50,  500, 10, "");                         break;
-		case MAPINFO_TYPE_CTS:        GameType_ConfigureSliders(e, l, l2, _("Point limit:"),    50,  500, 10, "");                         break;
-		default:                      GameType_ConfigureSliders(e, l, l2, _("Frag limit:"),      5,  100,  5, "fraglimit_override");       break;
+		case MAPINFO_TYPE_CTF:        GameType_ConfigureSliders(e, l, _("Capture limit:"),   1,   20, 1, "capturelimit_override");     break;
+		case MAPINFO_TYPE_DOMINATION: GameType_ConfigureSliders(e, l, _("Point limit:"),    50,  500, 10, "g_domination_point_limit"); break;
+		case MAPINFO_TYPE_KEYHUNT:    GameType_ConfigureSliders(e, l, _("Point limit:"),   200, 1500, 50, "g_keyhunt_point_limit");    break;
+		case MAPINFO_TYPE_LMS:        GameType_ConfigureSliders(e, l, _("Lives:"),           3,   50,  1, "g_lms_lives_override");     break;
+		case MAPINFO_TYPE_RACE:       GameType_ConfigureSliders(e, l, _("Laps:"),            1,   25,  1, "g_race_laps_limit");        break;
+		case MAPINFO_TYPE_NEXBALL:    GameType_ConfigureSliders(e, l, _("Goals:"),           1,   50,  1, "g_nexball_goallimit");      break;
+		case MAPINFO_TYPE_ASSAULT:    GameType_ConfigureSliders(e, l, _("Point limit:"),    50,  500, 10, "");                         break;
+		case MAPINFO_TYPE_ONSLAUGHT:  GameType_ConfigureSliders(e, l, _("Point limit:"),    50,  500, 10, "");                         break;
+		case MAPINFO_TYPE_CTS:        GameType_ConfigureSliders(e, l, _("Point limit:"),    50,  500, 10, "");                         break;
+		default:                      GameType_ConfigureSliders(e, l, _("Frag limit:"),      5,  100,  5, "fraglimit_override");       break;
 	}
 	me.mapListBox.refilter(me.mapListBox);
 }
