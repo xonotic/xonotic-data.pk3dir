53ac12f2bc8e1446c3147f18c85715855b7b8fbd
Samual Lenks <samual@xonotic.org>
2013-11-24 23:20:28 -0500

    Update demos tab

--- qcsrc/menu/xonotic/dialog_multiplayer_media_demo.c
+++ qcsrc/menu/xonotic/dialog_multiplayer_media_demo.c
@@ -35,21 +35,25 @@ entity makeXonoticDemoBrowserTab()
 }
 void XonoticDemoBrowserTab_fill(entity me)
 {
-	entity e, dlist;
-
+	entity e;
+	entity dlist = makeXonoticDemoList();
+	
 	me.TR(me);
-		me.TD(me, 1, 4, e = makeXonoticCheckBox(0, "cl_autodemo", _("Automatically record demos while playing")));
-	me.TR(me);
-	me.TR(me);
-		me.TD(me, 1, 0.5, e = makeXonoticTextLabel(0, _("Filter:")));
-		me.TD(me, 1, 6, e = makeXonoticInputBox(0, string_null));
-			dlist = makeXonoticDemoList();
+		me.TD(me, 1, 0.6, e = makeXonoticTextLabel(1, _("Filter:")));
+		me.TD(me, 1, 2.8, e = makeXonoticInputBox(0, string_null));
 			e.onChange = DemoList_Filter_Change;
 			e.onChangeEntity = dlist;
 			dlist.controlledTextbox = e;
 
-	me.TR(me);
-		me.TD(me, me.rows - 4, me.columns, dlist);
+	me.gotoRC(me, 0, 3.6);
+		me.TD(me, 1, 1.6, e = makeXonoticCheckBox(0, "cl_autodemo", _("Auto record demos")));
+		me.TD(me, 1, 1, e = makeXonoticButton(_("Refresh"), '0 0 0'));
+			//e.onClick = DemoList_Filter_Change;
+			//e.onClickEntity = dlist;
+
+
+	me.gotoRC(me, 1.5, 0);
+		me.TD(me, me.rows - 2.5, me.columns, dlist);
 
 	me.gotoRC(me, me.rows - 1, 0);
 		me.TD(me, 1, me.columns / 2, e = makeXonoticButton(_("Timedemo"), '0 0 0'));
