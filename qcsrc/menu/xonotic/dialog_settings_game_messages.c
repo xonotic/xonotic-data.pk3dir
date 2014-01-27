6aecd12ea391eb726275905daf5e14c685df2262
Samual Lenks <samual@xonotic.org>
2014-01-27 18:28:23 -0500

    Add proper notification settings tab interface

--- qcsrc/menu/xonotic/dialog_settings_game_messages.c
+++ qcsrc/menu/xonotic/dialog_settings_game_messages.c
@@ -33,7 +33,7 @@ void XonoticGameMessageSettingsTab_fill(entity me)
 	// notification_CHOICE_CTF_CAPTURE_* CTF capture record
 	// notification_CHOICE_CTF_PICKUP_* CTF pickup verbose info
 	// notification_FRAG* Frag message verbose info
-	// notification_show_location Show location of pickups in death messages
+	// notification_show_location Show location of deaths in death messages
 	// notification_show_sprees Show spree information
 	// notification_INFO_ITEM_WEAPON* Show weapon notifications in console
 	// Enable con_notify
@@ -44,68 +44,100 @@ void XonoticGameMessageSettingsTab_fill(entity me)
 	// notification_ANNCE_ACHIEVEMENT* achievements toggle
 	// notification_ANNCE_KILLSTREAK* killstreak toggles
 
-	// General settings for the player
+	/// frag information
 	me.TR(me);
-		me.TD(me, 1, 1, e = makeXonoticTextLabel(0, _("General Settings:")));
+		me.TD(me, 1, 3, e = makeXonoticTextLabel(0.5, _("Frag Information")));
+			e.isBold = TRUE;
+			e.alpha = 0.5;
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_show_sprees", _("Display information about killing sprees")));
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 2.8, e = makeXonoticCheckBox(0, "notification_show_sprees_info_specialonly", _("Only display sprees if they are achievements")));
+			makeMulti(e, "notification_show_sprees_center_specialonly");
+			setDependent(e, "notification_show_sprees", 1, 1);
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 2.8, e = makeXonoticCheckBox(0, "notification_show_sprees_center", _("Show spree information in centerprints")));
+			setDependent(e, "notification_show_sprees", 1, 1);
+	me.TR(me);
+		me.TDempty(me, 0.2);
+		me.TD(me, 1, 2.8, e = makeXonoticCheckBoxEx(3, 0, "notification_show_sprees_info", _("Show spree information in death messages")));
+			setDependent(e, "notification_show_sprees", 1, 1);
+	#if 0
+	me.TR(me);
+		me.TDempty(me, 0.1);
+		me.TD(me, 1, 0.9, e = makeXonoticTextLabel(0, _("Sprees in info messages:")));
+			setDependent(e, "notification_show_sprees", 1, 1);
+		me.TD(me, 1, 2, e = makeXonoticTextSlider("notification_show_sprees_info"));
+			e.addValue(e, ZCTX(_("Disabled")), "0");
+			e.addValue(e, ZCTX(_("Target")), "1");
+			e.addValue(e, ZCTX(_("Attacker")), "2");
+			e.addValue(e, ZCTX(_("Both")), "3");
+			e.configureXonoticTextSliderValues(e);
+			setDependent(e, "notification_show_sprees", 1, 1);
+	#endif
+	me.TR(me);
+		me.TDempty(me, 0.4);
+		me.TD(me, 1, 2.6, e = makeXonoticCheckBox(0, "notification_show_sprees_info_newline", _("Print on a seperate line")));
+			setDependent(e, "notification_show_sprees", 1, 1);
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(2, 1, "notification_CHOICE_FRAG", _("Add extra frag information to centerprint when available")));
+			makeMulti(e, "notification_CHOICE_FRAGGED notification_CHOICE_TYPEFRAG notification_CHOICE_TYPEFRAGGED");
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_show_location", _("Add frag location to death messages when available")));
 
+	me.gotoRC(me, 8.5, 0); me.setFirstColumn(me, me.currentColumn);
+		me.TD(me, 1, 3, e = makeXonoticTextLabel(0.5, _("Gamemode Settings")));
+			e.isBold = TRUE;
+			e.alpha = 0.5;
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticCheckBox(0, "notification_allow_chatboxprint", _("Print all notifications into the chatbox")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(2, 1, "notification_CHOICE_CTF_CAPTURE_TIME_RED", _("Display capture times in Capture The Flag")));
+			makeMulti(e, "notification_CHOICE_CTF_CAPTURE_TIME_BLUE notification_CHOICE_CTF_CAPTURE_BROKEN_RED notification_CHOICE_CTF_CAPTURE_BROKEN_BLUE notification_CHOICE_CTF_CAPTURE_UNBROKEN_RED notification_CHOICE_CTF_CAPTURE_UNBROKEN_BLUE ");
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticTextLabel(0, _("Notice connecting players:")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(2, 1, "notification_CHOICE_CTF_PICKUP_ENEMY", _("Display name of flag stealer in Capture The Flag")));
+			makeMulti(e, "notification_CHOICE_CTF_PICKUP_TEAM");
+
+	me.gotoRC(me, 0, 3.2); me.setFirstColumn(me, me.currentColumn);
+		me.TD(me, 1, 3, e = makeXonoticTextLabel(0.5, _("Other")));
+			e.isBold = TRUE;
+			e.alpha = 0.5;
 	me.TR(me);
-		me.TDempty(me, 0.3);
-		me.TD(me, 1, 3, e = makeXonoticTextSlider("notification_INFO_QUIT_DISCONNECT"));
-			//makeMulti(me, "notification_INFO_QUIT_KICK_IDLING notification_INFO_QUIT_KICK_SPECTATING notification_INFO_JOIN_PLAY notification_INFO_JOIN_CONNECT_TEAM_BLUE notification_INFO_JOIN_CONNECT_TEAM_PINK notification_INFO_JOIN_CONNECT_TEAM_RED notification_INFO_JOIN_CONNECT_TEAM_YELLOW");
-			e.addValue(e, ZCTX(_("off")), "0");
-			e.addValue(e, ZCTX(_("only console")), "1");
-			e.addValue(e, ZCTX(_("console & chat")), "2");
-			e.configureXonoticTextSliderValues(e);
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(4, 1, "con_notify", _("Display console messages in the top left corner")));
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticCheckBox(0, "notification_ITEM_WEAPON_DONTHAVE", _("Item notifications")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(2, 1, "notification_allow_chatboxprint", _("Display all info messages in the chatbox")));
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticCheckBox(0, "notification_CENTER_POWERUP_INVISIBILITY", _("Powerup notifications")));
-			//makeMulti(e, "notification_CENTER_POWERUP_SHIELD notification_CENTER_POWERUP_SPEED notification_CENTER_POWERUP_STRENGTH notification_CENTER_POWERDOWN_INVISIBILITY notification_CENTER_POWERDOWN_SHIELD notification_CENTER_POWERDOWN_SPEED notification_CENTER_POWERDOWN_STRENGTH notification_CENTER_SUPERWEAPON_BROKEN notification_CENTER_SUPERWEAPON_LOST notification_CENTER_SUPERWEAPON_PICKUP");	
-	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_frag_verbose", _("Additional information on frags")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(2, 1, "notification_INFO_QUIT_DISCONNECT", _("Display player statuses in the chatbox")));
+			makeMulti(e, "notification_INFO_QUIT_KICK_IDLING notification_INFO_JOIN_CONNECT_TEAM_BLUE notification_INFO_JOIN_CONNECT_TEAM_PINK notification_INFO_JOIN_CONNECT_TEAM_RED notification_INFO_JOIN_CONNECT_TEAM_YELLOW");
 	me.TR(me);
 	me.TR(me);
-	
-	// Gamemode specific notifications
-		me.TD(me, 1, 2, e = makeXonoticTextLabel(0, _("Gamemode notifications:")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_CENTER_POWERUP_INVISIBILITY", _("Powerup notifications")));
+			makeMulti(e, "notification_CENTER_POWERUP_SHIELD notification_CENTER_POWERUP_SPEED notification_CENTER_POWERUP_STRENGTH notification_CENTER_POWERDOWN_INVISIBILITY notification_CENTER_POWERDOWN_SHIELD notification_CENTER_POWERDOWN_SPEED notification_CENTER_POWERDOWN_STRENGTH notification_CENTER_SUPERWEAPON_BROKEN notification_CENTER_SUPERWEAPON_LOST notification_CENTER_SUPERWEAPON_PICKUP notification_INFO_POWERUP_INVISIBILITY notification_INFO_POWERUP_SHIELD notification_INFO_POWERUP_SPEED notification_INFO_POWERUP_STRENGTH");
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_CENTER_ARENA_BEGIN", _("Centerprint for gamemode notifications")));
-			//makeMulti(me, "notification_CENTER_ARENA_NEEDPLAYER notification_CENTER_ARENA_ROUNDSTART notification_CENTER_ASSAULT_ATTACKING notification_CENTER_ASSAULT_DEFENDING notification_CENTER_CTF_PICKUP_BLUE notification_CENTER_CTF_PICKUP_ENEMY notification_CENTER_CTF_PICKUP_RED notification_CENTER_CTF_PICKUP_TEAM notification_CENTER_FREEZETAG_FREEZE notification_CENTER_FREEZETAG_FROZEN notification_CENTER_FREEZETAG_REVIVE notification_CENTER_FREEZETAG_REVIVED notification_CENTER_FREEZETAG_ROUND_WIN_BLUE notification_CENTER_FREEZETAG_ROUND_WIN_PINK notification_CENTER_FREEZETAG_ROUND_WIN_RED notification_CENTER_FREEZETAG_ROUND_WIN_YELLOW notification_CENTER_FREEZETAG_SELF notification_CENTER_FREEZETAG_SPAWN_LATE notification_CENTER_KEEPAWAY_DROPPED notification_CENTER_KEEPAWAY_PICKUP notification_CENTER_KEEPAWAY_WARN notification_CENTER_KEYHUNT_HELP notification_CENTER_KEYHUNT_INTERFERE_BLUE notification_CENTER_KEYHUNT_INTERFERE_PINK notification_CENTER_KEYHUNT_INTERFERE_RED notification_CENTER_KEYHUNT_INTERFERE_YELLOW notification_CENTER_KEYHUNT_MEET notification_CENTER_KEYHUNT_SCAN notification_CENTER_KEYHUNT_START_BLUE notification_CENTER_KEYHUNT_START_PINK notification_CENTER_KEYHUNT_START_RED notification_CENTER_KEYHUNT_START_YELLOW notification_CENTER_KEYHUNT_WAIT notification_CENTER_LMS_CAMPCHECK");
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_CENTER_ITEM_WEAPON_DONTHAVE", _("Weapon centerprint notifications")));
+			makeMulti(e, "notification_CENTER_ITEM_WEAPON_DROP notification_CENTER_ITEM_WEAPON_GOT notification_CENTER_ITEM_WEAPON_NOAMMO notification_CENTER_ITEM_WEAPON_PRIMORSEC notification_CENTER_ITEM_WEAPON_UNAVAILABLE"); 
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_ctf_capture_verbose", _("Additional information on flag captures")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBox(0, "notification_INFO_ITEM_WEAPON_DONTHAVE", _("Weapon info message notifications")));
+			makeMulti(e, "notification_INFO_ITEM_WEAPON_DROP notification_INFO_ITEM_WEAPON_GOT notification_INFO_ITEM_WEAPON_NOAMMO notification_INFO_ITEM_WEAPON_PRIMORSEC notification_INFO_ITEM_WEAPON_UNAVAILABLE"); 
+
+	me.gotoRC(me, 8.5, 3.2); me.setFirstColumn(me, me.currentColumn);
+		me.TD(me, 1, 3, e = makeXonoticTextLabel(0.5, _("Announcers")));
+			e.isBold = TRUE;
+			e.alpha = 0.5;
+	#if 0
+	// there's just not enough room for this, and it's not important enough to justify...
 	me.TR(me);
-	
-	// Information on killingsprees
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(2, 0, "notification_ANNCE_NUM_RESPAWN_1", _("Respawn countdown sounds")));
+			makeMulti(e, "notification_ANNCE_NUM_RESPAWN_2 notification_ANNCE_NUM_RESPAWN_3 notification_ANNCE_NUM_RESPAWN_4 notification_ANNCE_NUM_RESPAWN_5 notification_ANNCE_NUM_RESPAWN_6 notification_ANNCE_NUM_RESPAWN_7 notification_ANNCE_NUM_RESPAWN_8 notification_ANNCE_NUM_RESPAWN_9 notification_ANNCE_NUM_RESPAWN_10");
+	#endif
 	me.TR(me);
-		me.TD(me, 1, 2, e = makeXonoticTextLabel(0, _("Information on killingsprees:")));
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(1, 0, "notification_ANNCE_KILLSTREAK_03", _("Killstreak sounds")));
+			makeMulti(e, "notification_ANNCE_KILLSTREAK_05 notification_ANNCE_KILLSTREAK_10 notification_ANNCE_KILLSTREAK_15 notification_ANNCE_KILLSTREAK_20 notification_ANNCE_KILLSTREAK_25 notification_ANNCE_KILLSTREAK_30");
 	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticCheckBox(0, "notification_show_sprees", _("Print information on sprees")));
-	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticTextLabel(0, _("Show spree information:")));
-	me.TR(me);
-		me.TDempty(me, 0.3);
-		me.TD(me, 1, 3, e = makeXonoticTextSlider("notification_show_sprees_info"));
-			e.addValue(e, ZCTX(_("off")), "0");
-			e.addValue(e, ZCTX(_("target")), "1");
-			e.addValue(e, ZCTX(_("attacker")), "2");
-			e.addValue(e, ZCTX(_("target & attacker")), "3");
-			e.configureXonoticTextSliderValues(e);
-	me.TR(me);
-		me.TDempty(me, 0.1);
-		me.TD(me, 1, 2, e = makeXonoticCheckBox(0, "notification_show_sprees_info_newline", _("Print spree information in a new line")));
-		
+		me.TD(me, 1, 3, e = makeXonoticCheckBoxEx(1, 0, "notification_ANNCE_ACHIEVEMENT_AIRSHOT", _("Achievement sounds")));
+			makeMulti(e, "notification_ANNCE_ACHIEVEMENT_AMAZING notification_ANNCE_ACHIEVEMENT_AWESOME notification_ANNCE_ACHIEVEMENT_BOTLIKE notification_ANNCE_ACHIEVEMENT_ELECTROBITCH notification_ANNCE_ACHIEVEMENT_IMPRESSIVE notification_ANNCE_ACHIEVEMENT_YODA");
+
+	me.gotoRC(me, me.rows - 1, 0);
+		me.TD(me, 1, me.columns, makeXonoticCommandButton(_("Apply immediately"), '0 0 0', "echo stuff", COMMANDBUTTON_APPLY));
 }
 #endif
