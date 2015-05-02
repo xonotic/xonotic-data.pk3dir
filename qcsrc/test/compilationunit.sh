7efb90d3f4f1f73636764ee8ab21f667d1b49a05
TimePath <andrew.hardaker1995@gmail.com>
2015-05-02 13:14:27 +1000

    Sort #includes

--- qcsrc/test/compilationunit.sh
+++ qcsrc/test/compilationunit.sh
@@ -3,18 +3,16 @@ set -eu
 cd "$(dirname "$0")"
 cd ..
 
-export QCC=../../../gmqcc/gmqcc
+declare QCC=../../../gmqcc/gmqcc
 
 function check() {
   declare -l base="$1"
   declare -la predefs=("${!2}")
-  find "$base" -type f -name '*.qc' -print0 | while IFS= read -r -d '' file; do
+  find "$base" -type f -name '*.qc' -print0 | sort -z | while IFS= read -r -d '' file; do
     echo "$file"
-    ${QCC} -std=gmqcc -fftepp ${predefs[@]} "$file" >/dev/null
+    ${QCC} -std=gmqcc -fftepp -fftepp-predefs -futf8 -O3 "${predefs[@]}" "$file" >/dev/null
   done
 }
-
-# TODO: `sort -V` all the includes
 
 clientdefs=("-DCSQC" "common/util-pre.qh" "dpdefs/csprogsdefs.qh")
 check client clientdefs[@]
