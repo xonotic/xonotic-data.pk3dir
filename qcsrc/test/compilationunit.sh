fc82c845c5bcd6b0c837ed82db072b0b7eba0239
TimePath <andrew.hardaker1995@gmail.com>
2015-05-03 15:00:21 +1000

    Cleanup vehicle #includes

--- qcsrc/test/compilationunit.sh
+++ qcsrc/test/compilationunit.sh
@@ -8,6 +8,10 @@ declare -a NOWARN=(
   '-Wno-unused-variable'
   '-Wno-implicit-function-pointer'
 )
+declare -a FEATURES=(
+  '-DVEHICLES_ENABLED=1'
+  '-DVEHICLES_USE_ODE=0'
+)
 declare QCC=../../../gmqcc/gmqcc
 
 function check() {
@@ -15,7 +19,7 @@ function check() {
   declare -la predefs=("${!2}")
   find "$base" -type f -name '*.qc' -print0 | sort -z | while IFS= read -r -d '' file; do
     echo "$file"
-    ${QCC} -std=gmqcc -fftepp -fftepp-predefs -Werror -Wall "${NOWARN[@]}" -futf8 -O3 "${predefs[@]}" "$file" >/dev/null
+    ${QCC} -std=gmqcc -fftepp -fftepp-predefs -Werror -Wall "${NOWARN[@]}" "${FEATURES[@]}" -futf8 -O3 "${predefs[@]}" "$file" >/dev/null
   done
 }
 
