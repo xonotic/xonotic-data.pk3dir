65e9ceb03c81bc515d090a54f65dd620c8c6dc43
TimePath <andrew.hardaker1995@gmail.com>
2016-08-21 16:01:29 +1000

    Ensure headers are always #included

--- qcsrc/tools/headerstyle.sh
+++ qcsrc/tools/headerstyle.sh
@@ -17,7 +17,7 @@ function check() {
     find "$base" -type f -name '*.qc' -print0 | sort -z | while read -r -d '' file; do
         echo "$file"
         declare -l file_h="${file%.qc}.qh"
-        if [ ! -f "$file_h" ]; then echo "#pragma once" > "$file_h"; fi
+        if [[ ! -f "$file_h" ]]; then echo "#pragma once" > "$file_h"; fi
 
         include=$(basename "$file")
         include="${include%.qc}.qh"
@@ -28,21 +28,9 @@ function check() {
         echo "$file"
         startswith "$file" "#pragma once"
     done
-    find "$base" -type f -name '_mod.inc' -print0 | sort -z | while read -r -d '' file; do
-        echo "$file"
-        declare -l file_inc="${file%_mod.inc}_all.inc"
-        if [ ! -f "$file_inc" ]; then
-            echo '#include "_all.qh"' > "$file_inc"
-            echo '#include "_mod.inc"' >> "$file_inc"
-        fi
-        declare -l file_qh="${file%_mod.inc}_all.qh"
-        if [ ! -f "$file_qh" ]; then
-            echo '#pragma once' > "$file_qh"
-            echo '#include "_mod.qh"' >> "$file_qh"
-        fi
-    done
 }
 
 check client
 check server
 check menu
+check common
