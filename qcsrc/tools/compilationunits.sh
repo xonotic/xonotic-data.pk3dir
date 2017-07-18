63f34ee151e32cd041437201a6b8fecf8971a928
Martin Taibr <taibr.martin@gmail.com>
2017-07-18 17:59:37 +0200

    document compilationunits.sh

--- qcsrc/tools/compilationunits.sh
+++ qcsrc/tools/compilationunits.sh
@@ -2,9 +2,13 @@
 set -eu
 cd ${0%/*}
 
-# if these files exist from previous compilation, `./all compile` will stop
+# This script attempts to build the codebase in every possible header configuration,
+# to check that all files #include what they need, so that we can eventually move away
+# from a unity build and into incremental compilation.
+
+# If these files exist from previous compilation, `./all compile` will stop
 # detecting changes after running this script so delete them to trigger
-# a recompile next time
+# a recompile next time.
 if [ -f ../../csprogs.dat ]; then
     rm ../../csprogs.dat
 fi
