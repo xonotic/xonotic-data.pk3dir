31d3747b97ae28f07e88ededa4d28cb287c6a5dc
drjaska <drjaska83@gmail.com>
2025-01-15 01:52:11 +0200

    Add a summary to scripts-diff-test-wrapper.sh

--- qcsrc/tools/scripts-diff-test-wrapper.sh
+++ qcsrc/tools/scripts-diff-test-wrapper.sh
@@ -3,6 +3,25 @@
 set -eu
 
 
+# This script is a wrapper for running other scripts to test whether
+# or not they produce a change, such as a file modification,
+# creation, or deletion.
+#
+# In order to test it accurately this # script should not be
+# ran with uncommitted changes.
+#
+# In the case that there are changes produced this wrapper script
+# mentions that there has been a change and exits with an error.
+#
+# Any of the potential changes are left in the git index so that
+# if this script is run in a developer setup they can easily see
+# the changes and commit them or address them without the need
+# to re-run any scripts.
+#
+# This script is also intended for GitLab CI/CD pipelines so that
+# we're able to have tests which track the output of certain scripts.
+
+
 # Check that we got an argument
 if [ -z "${1-}" ]
 then
